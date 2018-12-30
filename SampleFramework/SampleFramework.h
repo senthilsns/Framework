e44273323939331b59973ce68a3ff552d7cc0291
Senthil kumar K <nightrider.cbe@gmail.com>
2018-12-30 23:11:00 +0530

    commit

---
+++ SampleFramework/SampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleFramework.h
+//  SampleFramework
+//
+//  Created by SENTHILKUMAR on 30/12/18.
+//  Copyright © 2018 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleFramework.
+FOUNDATION_EXPORT double SampleFrameworkVersionNumber;
+
+//! Project version string for SampleFramework.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramework/PublicHeader.h>
+
+
